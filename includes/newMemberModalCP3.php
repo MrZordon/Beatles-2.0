2f552370d3719ef82423a975748ac79863c7b4a1
MrZordon <hovsepianp@merrimack.edu>
2014-05-08 19:08:54 -0400

    Update newMemberModalCP3.php

--- includes/newMemberModalCP3.php
+++ includes/newMemberModalCP3.php
@@ -1,53 +1,52 @@
-
-		<div id="NewMemberModalCP3" class="reveal-modal" data-reveal>
+<div id="NewMemberModalCP3" class="reveal-modal" data-reveal>
 		<h2>Domestic Violence History</h2>
-		<form>
-	  
+	
+
 			<div class="row">
 
 				<div class="large-3 columns"> 
 					<label>Have you been a victim of Domestic Violence:</label> 
-					<input type="radio" name="violenceYN" value="Yes" id="yes1">
+					<input type="radio" name="violenceYN_Domestic" value="Yes" id="violenceYNDomestic">
 						<label for="yes1">Yes</label> 
-					<input type="radio" name="violenceYN" value="No" id="no1">
+					<input type="radio" name="violenceYN_Domestic" value="No" id="violenceYNDomestic">
 						<label for="no1">No</label> 	
 				</div>
-				
+
 				<div class="large-3 columns"> 
 					<label>If so, do you have a safety plan:</label> 
-					<input type="radio" name="safetyYN" value="Yes" id="yes2">
+					<input type="radio" name="safetyYN_Domestic" value="Yes" id="safetyYNDomestic">
 						<label for="yes2">Yes</label> 
-					<input type="radio" name="safetyYN" value="No" id="no2">
+					<input type="radio" name="safetyYN_Domestic" value="No" id="safetyYNDomestic">
 						<label for="no2">No</label> 	
 				</div>
-				
+
 				<div class="large-6 columns"> 
 					<label>If so, is there a restraining order:</label> 
-					<input type="radio" name="restrainingOrderYN" value="Yes" id="yes3">
+					<input type="radio" name="restrainingOrderYN_Domestic" value="Yes" id="restrainingOrderYNDomestic">
 						<label for="yes3">Yes</label> 
-					<input type="radio" name="restrainingOrderYN" value="No" id="no3">
+					<input type="radio" name="restrainingOrderYN_Domestic" value="No" id="restrainingOrderYNDomestic">
 						<label for="no3">No</label> 	
 				</div>
-							
+
 			</div>
-			
+
 			<div class="row">
 				<div class="large-12 columns">
-					<label>Name of Perpetrator:<input type="text" placeholder="Custody Issues" /> </label> 
+					<label>Name of Perpetrator:<input type="text" name="perpName_Domestic" id="perpNameDomestic" placeholder="Full Name" /> </label> 
 				</div>
 			</div>
-			
-		</form>
+		
 		<div class="row">
-			<div class="large-3 columns"> 
-				<p><a href="#" data-reveal-id="NewMemberModalCP4" class="secondary button">Next</a></p>
+			<div class="large-3 columns">				
+				<button id="DomesticInfo" onclick="toggle_visibility('hideShow3');">Save Information</button>	
+				<p><button href="#" data-reveal-id="NewMemberModalCP4" class="secondary button" id="hideShow3"  style="display:none;">Next</buttonW></p>
 			</div>
 			<div class="large-9 columns">
 				<h4>Progress</h4>
 				<div class="progress large-9 round"> 
-					<span class="meter" style="width: 30%"></span> 
+					<span class="meter" style="width: 20%"></span> 
 				</div>
 			</div>
-		<div>
+		</div>
 	  <a class="close-reveal-modal">&#215;</a>
 	</div>
