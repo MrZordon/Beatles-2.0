d49635887e448549d91497b329cba5cc042e4618
MrZordon <hovsepianp@merrimack.edu>
2014-05-08 20:00:36 -0400

    Update newMemberModalCP4.php

--- includes/newMemberModalCP4.php
+++ includes/newMemberModalCP4.php
@@ -1,76 +1,56 @@
-
-		<div id="NewMemberModalCP4" class="reveal-modal" data-reveal>
-		<h2>Personal Information</h2>
-		<form>
-	  
-			<div class="row">		
-				<div class="large-12 columns">
-					<label>Tell Us about your Family: <textarea name="familyDetails" placeholder="Tell Us about your Family"> </textarea> </label> 
-				</div>
+	<div id="NewMemberModalCP4" class="reveal-modal" data-reveal>
+		
+		<h2>Employment</h2>
+	
+			<div class="row"> 
+					<div class="large-4 columns"> 
+						<label>Currently employed?</label> 
+								<input type="radio" name="agreementYN_Employment" value="Yes" id="agreementYNEmployment">
+									<label for="agreementY">Yes</label> 
+								<input type="radio" name="agreementYN_Employment" value="No" id="agreementYNEmployment">
+									<label for="agreementN">No</label> 	
+					</div> 
 			</div>
-			<div class="row">		
-				<div class="large-12 columns">
-					<label>How did you become homeless?: <textarea name="homelessDetails" placeholder="How did you become homeless?"> </textarea> </label> 
-				</div>
-			</div>
-			<div class="row">		
-				<div class="large-12 columns">
-					<label>Describe your housing history: <textarea name="housingDetails" placeholder="Describe your housing history"> </textarea> </label> 
-				</div>
-			</div>
-			
-			<div class="row">
-
-				<div class="large-4 columns"> 
-					<label>Have you ever lived on your own?</label> 
-					<input type="radio" name="livedAloneYN" value="Yes" id="livedY">
-						<label for="livedY">Yes</label> 
-					<input type="radio" name="livedAloneYN" value="No" id="livedN">
-						<label for="livedN">No</label> 	
-				</div>
-				<div class="large-4 columns"> 
-					<label>Were you responsible for rent?</label> 
-					<input type="radio" name="rentYN" value="Yes" id="rentY">
-						<label for="rentY">Yes</label> 
-					<input type="radio" name="rentYN" value="No" id="rentN">
-						<label for="rentN">No</label> 	
-				</div>
-				<div class="large-4 columns">
-					
-				</div>
-			</div>
-			<div class="row">		
-				<div class="large-12 columns">
-					<label>What would your landlord say about your ability to abide by a lease?<textarea name="landlordComments" placeholder="What would your landlord say about your ability to abide by a lease?"> </textarea> </label> 
-				</div>
-			</div>
-			<div class="row">
-				<div class="large-4 columns"> 
-					<label>Have you applied for Section 8?</label> 
-					<input type="radio" name="section8YN" value="Yes" id="section8Y">
-						<label for="section8Y">Yes</label> 
-					<input type="radio" name="section8YN" value="No" id="section8N">
-						<label for="section8N">No</label> 	
-				</div>
-				<div class="large-4 columns"> 
-					<label>If so, where?<input type="text" name="where" placeholder="Where" /></label> 
-				</div>
-				<div class="large-4 columns"> 
-					<label>How Long is the waiting list?<input type="text" name="waitListLength" placeholder="How Long is the waiting list" /></label> 
-				</div>
-			</div>
-			
-		</form>
+			<div class="row"> 
+					<div class="large-4 columns"> 
+						<label>Company Name: <input type="text" id="companyNameEmployment" name="companyName_Employment" placeholder="Company Name" /> </label> 
+					</div> 
+					<div class="large-4 columns"> 
+						<label>Manager's Name: <input type="text" id="managerNameEmployment" name="managerName_Employment" placeholder="Manager's Name" /> </label> 
+					</div>
+					<div class="large-4 columns"> 
+						<label>Manager's Phone Number:<input type="text" id="managerPhoneNumEmployment" name="managerPhoneNum_Employment" placeholder="xxx-xxx-xxxx" /> </label> 
+					</div>
+			</div> 
+			<div class="row"> 
+					<div class="large-4 columns"> 
+						<label>Hours From: <input type="text" name="hoursFrom_Employment" id="hoursFromEmployment" /> </label> 
+					</div> 
+					<div class="large-4 columns"> 
+						<label>Hours To: <input type="text"  name="hoursTo_Employment" id="hoursToEmployment"  /> </label> 
+					</div>
+					<div class="large-4 columns"> 
+						<label>Rate of Pay:<input type="text" name="rateOfPay_Employment" id="rateOfPayEmployment" /> </label> 
+					</div>
+				</div> 
+				<div class="row"> 
+					<div class="large-4 columns"> 
+							<label>Income:<input type="text" name="income_Employment" id="incomeEmployment" /> </label> 
+						</div>
+					</div>
+		
 		<div class="row">
-			<div class="large-3 columns"> 
-				<p><a href="#" data-reveal-id="NewMemberModalCP5" class="secondary button">Next</a></p>
+			<div class="large-3 columns">				
+				<button id="EmploymentSubmit" onclick="toggle_visibility('hideShow4');">Save Information</button>	
+				<p><button href="#" data-reveal-id="NewMemberModalCP5" class="secondary button" id="hideShow4"  style="display:none;">Next</buttonW></p>
 			</div>
 			<div class="large-9 columns">
 				<h4>Progress</h4>
 				<div class="progress large-9 round"> 
-					<span class="meter" style="width: 40%"></span> 
+					<span class="meter" style="width: 20%"></span> 
 				</div>
 			</div>
-		<div>
+		</div>
+		
 	  <a class="close-reveal-modal">&#215;</a>
 	</div>
