7b35ff130c9ac527b764bc3914ff50b529715417
MrZordon <hovsepianp@merrimack.edu>
2014-05-09 00:09:41 -0400

    Update newMemberModalCP6.php

--- includes/newMemberModalCP6.php
+++ includes/newMemberModalCP6.php
@@ -5,26 +5,26 @@
 					<div class="row">
 						<div class=large-6 columns">
 							<label>Has the resident ever been incarcerated?</label> 
-									<input type="radio" name="incarcerationYN_Incarceration" value="Yes" id="incarcerationYNIncarceration">
+									<input type="radio" name="incarceration_YN" value="Yes" id="incarcerationYN">
 										<label for="incarcerationY">Yes</label> 
-									<input type="radio" name="incarcerationYN_Incarceration" value="No" id="incarcerationYNIncarceration">
+									<input type="radio" name="incarceration_YN" value="No" id="incarcerationYN">
 										<label for="incarcerationN">No</label> 
 							</div>
 					</div>
 					<div class="row">
 								<div class="large-5 columns"> 
-									<label>Incarceration Location: <input type="text" id="incarcerationLocIncarceration" name="incarcerationLoc_Incarceration"  /> </label> 
+									<label>Incarceration Location: <input type="text" id="incarcerationLoc" name="incarceration_Loc"  /> </label> 
 								</div> 
 								<div class="large-5 columns"> 
-									<label>Incarceration Charges: <input type="text" id="incarcerationChargesIncarceration" name="incarcerationCharges_Incarceration" /> </label> 
+									<label>Incarceration Charges: <input type="text" id="incarcerationCharges" name="incarceration_Charges" /> </label> 
 								</div> 
 						</div>
 						<div class="row">
 								<div class="large-5 columns"> 
-									<label>Incarceration Start Date: <input type="text" id="incarcerationStartIncarceration" name="incarcerationStart_Incarceration" placeholder="mm/dd/yyyy" /> </label> 
+									<label>Incarceration Start Date: <input type="text" id="incarcerationStart" name="incarceration_Start" placeholder="mm/dd/yyyy" /> </label> 
 								</div> 
 								<div class="large-5 columns"> 
-									<label>Incarceration End Date: <input type="text" id="incarcerationEndIncarceration" name="incarcerationEnd_Incarceration"  placeholder="mm/dd/yyyy"/> </label> 
+									<label>Incarceration End Date: <input type="text" id="incarcerationEnd" name="incarceration_End"  placeholder="mm/dd/yyyy"/> </label> 
 								</div> 
 						</div>
 						<h2> Institutionalizations</h2>
@@ -32,33 +32,33 @@
 					<div class="row">
 						<div class=large-6 columns">
 							<label>Has the resident ever been institutionalized?</label> 
-									<input type="radio" name="institutionalizedYN_Incarceration" value="Yes" id="institutionalizedYNIncarceration">
+									<input type="radio" name="institutionalized_YN" value="Yes" id="institutionalizedYN">
 										<label for="institutionalizedY">Yes</label> 
-									<input type="radio" name="institutionalizedYN_Incarceration" value="No" id="institutionalizedYNIncarceration">
+									<input type="radio" name="institutionalized_YN" value="No" id="institutionalizedYN">
 										<label for="institutionalizedN">No</label> 
 							</div>
 					</div>
 					<div class="row">
 								<div class="large-5 columns"> 
-									<label>Name of Institution: <input type="text" id="institutionNameIncarceration" name="institutionName_Incarceration"  /> </label> 
+									<label>Name of Institution: <input type="text" id="institutionName" name="institution_Name"  /> </label> 
 								</div> 
 								<div class="large-5 columns"> 
-									<label>Reasons for Institutionalization: <input type="text" id="institutionalizationReasonsIncarceration" name="institutionalizationReasons_Incarceration" /> </label> 
+									<label>Reasons for Institutionalization: <input type="text" id="institutionReason" name="institution_Reason" /> </label> 
 								</div> 
 						</div>
 						<div class="row">
 								<div class="large-5 columns"> 
-									<label>Institution Start Date: <input type="text" id="institutionStartIncarceration" name="institutionStart_Incarceration" placeholder="mm/dd/yyyy" /> </label> 
+									<label>Institution Start Date: <input type="text" id="institutionStart" name="institution_Start" placeholder="mm/dd/yyyy" /> </label> 
 								</div> 
 								<div class="large-5 columns"> 
-									<label>Institution Release Date: <input type="text" id="institutionEndIncarceration" name="institutionEnd_Incarceration"  placeholder="mm/dd/yyyy"/> </label> 
+									<label>Institution Release Date: <input type="text" id="institutionEnd" name="institution_End"  placeholder="mm/dd/yyyy"/> </label> 
 								</div> 
 						</div>
 		
 		<div class="row">
 			<div class="large-3 columns">				
 				<button id="IncarcerationSubmit" onclick="toggle_visibility('hideShow6);">Save Information</button>	
-				<p><button href="#" data-reveal-id="" class="secondary button" id="hideShow6"  style="display:none;">Submit</buttonW></p>
+				<p><button href="#" class="close-reveal-modal">Finish</buttonW></p>
 			</div>
 			<div class="large-9 columns">
 				<h4>Progress</h4>
